4812f53f4a6f63089b0b7591b45370204187c920
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-29 15:01:37 +0530

    scripts: add support for LTO builds
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- dronesetup.sh
+++ dronesetup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd ..
 
-git config --global user.name UtsavBalar1231
-git config --global user.email utsavbalar1231@gmail.com
-
 # Anykernl Flasher
 git clone https://github.com/UtsavBalar1231/AnyKernel3 -b master anykernel
 
@@ -12,7 +9,9 @@ if [[ "$@" =~ "clang"* ]]; then
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 gcc
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 gcc32
 	git clone https://github.com/BenzoRom/benzoClang-12.0  --depth=1 clang
-# ProtonClang-12.0
+# ProtonClang-12.0 LTO build
+elif [[ "$@" =~ "lto"* ]]; then
+	git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 clang
 elif [[ "$@" =~ "proton"* ]]; then
 	git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 clang
 # BareMetal GCC-10.2.0
--- kernel.sh
+++ kernel.sh
@@ -6,6 +6,8 @@ cd ..
 # Export compiler type
 if [[ "$@" =~ "clang"* ]]; then
 	export COMPILER="BenzoClang-12.0"
+elif [[ "$@" =~ "lto"* ]]; then
+	export COMPILER="ProtonClang-12.0 LTO"
 elif [[ "$@" =~ "proton"* ]]; then
 	export COMPILER="ProtonClang-12.0"
 elif [[ "$@" =~ "gcc"* ]]; then
@@ -16,13 +18,21 @@ fi
 
 # Export correct version
 if [[ "$@" =~ "beta"* ]]; then
-	export TYPE=beta
-	export VERSION="IMMENSiTY-AUTO-RAPHAEL-${DRONE_BUILD_NUMBER}"
+	if [[ "$@" =~ "lto"* ]]; then
+		export TYPE=beta-LTO
+	else
+                export TYPE=beta
+	fi
+	export VERSION="IMMENSiTY-AUTO-RAPHAEL-${TYPE}${DRONE_BUILD_NUMBER}"
 	export INC="$(echo ${RC} | grep -o -E '[0-9]+')"
 	INC="$((INC + 1))"
 else
-	export TYPE=C.I
-	export VERSION="IMMENSiTY-AUTO"
+	if [[ "$@" =~ "lto"* ]]; then
+		export TYPE=C.I-LTO
+	else
+                export TYPE=C.I
+	fi
+	export VERSION="IMMENSiTY-AUTO-RAPHAEL-${TYPE}"
 fi
 
 export ZIPNAME="${VERSION}.zip"
@@ -57,10 +67,41 @@ if [[ "$@" =~ "clang"* ]]; then
 		CROSS_COMPILE="aarch64-linux-android-" \
 		CROSS_COMPILE_ARM32="arm-linux-androideabi-" \
 		-j${KEBABS}
-elif [[ "$@" =~ "proton"* ]]; then
+elif [[ "$@" =~ "lto"* ]]; then
 scripts/config --file out/.config \
 	        -e LTO \
 	        -e LTO_CLANG \
+		-d THINLTO \
+	        -e SHADOW_CALL_STACK \
+	        -e TOOLS_SUPPORT_RELR \
+	        -e LD_LLD
+cd out
+
+make O=out \
+        ARCH=arm64 \
+        olddefconfig
+cd ../
+
+PATH=/drone/src/clang/bin/:$PATH
+	make ARCH=arm64 \
+		O=out \
+		CC="clang" \
+		LD="ld.lld" \
+		AR="llvm-ar" \
+		NM="llvm-nm" \
+		OBJCOPY="llvm-objcopy" \
+		OBJDUMP="llvm-objdump" \
+	        OBJSIZE="llvm-size" \
+		READELF="llvm-readelf" \
+		STRIP="llvm-strip" \
+		CLANG_TRIPLE="aarch64-linux-gnu-" \
+		CROSS_COMPILE="aarch64-linux-gnu-" \
+		CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+		-j${KEBABS}
+elif [[ "$@" =~ "proton"* ]]; then
+scripts/config --file out/.config \
+	        -d LTO \
+	        -d LTO_CLANG \
 		-d THINLTO \
 	        -e SHADOW_CALL_STACK \
 	        -e TOOLS_SUPPORT_RELR \
