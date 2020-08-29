72bc23aa1b8b1d86ac5d688a98ede524a98f3d78
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-29 13:00:04 +0530

    scripts: dronesetup: update github username
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- dronesetup.sh
+++ dronesetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 
-git clone https://github.com/UtsavisGreat/AnyKernel3 -b master anykernel
+git clone https://github.com/UtsavBalar/AnyKernel3 -b master anykernel
 
 if [[ "$@" =~ "clang"* ]]; then
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 gcc
