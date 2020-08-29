0ba02c513f6cd8b2dbe28348051f8ebb6b1a48a3
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-29 12:58:04 +0530

    scripts: refactor for current base
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- dronesetup.sh
+++ dronesetup.sh
@@ -8,7 +8,7 @@ if [[ "$@" =~ "clang"* ]]; then
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth=1 gcc32
 	git clone https://github.com/BenzoRom/benzoClang-10.0  --depth=1 clang
 elif [[ "$@" =~ "dragon"* ]]; then
-	wget https://github.com/kdrag0n/proton-clang-build/releases/download/20190803/proton_clang-20190803-c835164a.tar.zst && tar -I zstd -xvf proton_clang-20190803-c835164a.tar.zst && mv proton_clang-20190803-c835164a dragontc
+        git clone https://github.com/kdrag0n/proton-clang --depth=1 clang
 elif [[ "$@" =~ "gcc9"* ]]; then
         git clone https://github.com/kdrag0n/aarch64-elf-gcc -b  9.x --depth=1 gcc
 	git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 gcc32
--- kernel.sh
+++ kernel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# We're building IMMENSITY.
+# We're building IMMENSiTY.
 cd ..
 
 # Export compiler type
 if [[ "$@" =~ "clang"* ]]; then
 	export COMPILER="Clang 9.0.6"
 elif [[ "$@" =~ "dragon"* ]]; then
-        export COMPILER="DragonTC-10.0"
+        export COMPILER="ProtonClang-12.0"
 elif [[ "$@" =~ "gcc9"* ]]; then
         export COMPILER="Bare Metal GCC-9.2.0"
 else
@@ -17,12 +17,12 @@ fi
 # Export correct version
 if [[ "$@" =~ "beta"* ]]; then
 	export TYPE=beta
-	export VERSION="IMMENSITY-BETA-KERNEL-${DRONE_BUILD_NUMBER}-${RELEASE_CODENAME}"
+	export VERSION="IMMENSiTY-AUTO-RAPHAEL-${DRONE_BUILD_NUMBER}"
 	export INC="$(echo ${RC} | grep -o -E '[0-9]+')"
 	INC="$((INC + 1))"
 else
 	export TYPE=stable
-	export VERSION="IMMENSITY-KERNEL-${RELEASE_CODENAME}"
+	export VERSION="IMMENSiTY-AUTO"
 fi
 
 export ZIPNAME="${VERSION}.zip"
@@ -35,25 +35,46 @@ fi
 
 # Post to CI channel
 curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/SendAnimation -d animation=https://thumbs.gfycat.com/TidyOccasionalIncatern-size_restricted.gif -d chat_id=${CI_CHANNEL_ID}
-curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Kernel: <code>iMMENSITY Kernel</code>
+curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Kernel: <code>IMMENSiTY KERNAL</code>
 Type: <code>${TYPE}</code>
 Device: <code>XiaoMi Redmi K20 Pro (raphael)</code>
 Compiler: <code>${COMPILER}</code>
 Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
 <i>Build started on Drone Cloud...</i>
-Check the build status here: https://cloud.drone.io/UtsavisGreat/kernel_xiaomi_raphael/${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
+Check the build status here: https://cloud.drone.io/UtsavBalar1231/kernel_xiaomi_raphael/${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
 curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build started for revision ${DRONE_BUILD_NUMBER}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
 
 # Make is shit so I have to pass thru some toolchains
 # Let's build, anyway
-PATH="/drone/src/clang/bin:${PATH}"
+PATH=/drone/src/clang/bin:/drone/src/gcc/bin:/drone/src/gcc32/bin:${PATH}
 START=$(date +"%s")
 make O=out ARCH=arm64 raphael_defconfig
 if [[ "$@" =~ "clang"* ]]; then
-        make -j${KEBABS} O=out ARCH=arm64 CC=clang CLANG_TRIPLE="aarch64-linux-android-" CROSS_COMPILE="/drone/src/gcc/bin/aarch64-linux-android-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-linux-androideabi-"
+	make ARCH=arm64 \
+		O=out \
+		CC="ccache clang" \
+		CLANG_TRIPLE="aarch64-linux-gnu-" \
+		CROSS_COMPILE="aarch64-linux-android-" \
+		CROSS_COMPILE_ARM32="arm-linux-androidabi-" \
+		-j${KEBABS}
 elif [[ "$@" =~ "dragon"* ]]; then
-	PATH="/drone/src/dragontc/bin:${PATH}"
-	make -j${KEBABS} O=out ARCH=arm64 CROSS_COMPILE="/drone/src/dragontc/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/drone/src/dragontc/bin/arm-linux-gnueabi-"
+	PATH=/home/utsavthecunt/proton-clang/bin/:$PATH
+	make ARCH=arm64 \
+	        O=out \
+	        CC="ccache clang" \
+	        LLVM_IAS=1 \
+	        LD="ld.lld" \
+	        AR="llvm-ar" \
+	        NM="llvm-nm" \
+	        OBJCOPY="llvm-objcopy" \
+	        OBJDUMP="llvm-objdump" \
+	        OBJSIZE="llvm-size" \
+	        READELF="llvm-readelf" \
+	        STRIP="llvm-strip" \
+	        CLANG_TRIPLE="aarch64-linux-gnu-" \
+	        CROSS_COMPILE="aarch64-linux-gnu-" \
+	        CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+                -j${KEBABS}
 elif [[ "$@" =~ "gcc9"* ]]; then
         make -j${KEBABS} O=out ARCH=arm64 CROSS_COMPILE="/drone/src/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-eabi-"
 else
@@ -62,8 +83,8 @@ fi
 END=$(date +"%s")
 DIFF=$(( END - START))
 
-cp $(pwd)/out/arch/arm64/boot/Image.gz $(pwd)/anykernel
-cp $(pwd)/out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb $(pwd)/anykernel/dtbs
+cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/
+cp $(pwd)/out/arch/arm64/boot/dtbo.img $(pwd)/anykernel/
 
 # POST ZIP OR FAILURE
 cd anykernel
@@ -78,5 +99,5 @@ else
 	exit 1;
 fi
 
-rm -rf ${ZIPNAME} && rm -rf Image.gz
+rm -rf ${ZIPNAME} && rm -rf Image.gz-dtb
 
